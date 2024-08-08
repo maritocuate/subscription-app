dc315e55d1709d69e55e5c6e16db2ba02870af35
maritocuate <mario300@gmail.com>
2024-08-08 19:19:11 -0300

    ✨ kinde integration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+'use client'
+import { LoginLink } from '@kinde-oss/kinde-auth-nextjs'
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-3xl font-bold">Subscription App</h1>
+      <LoginLink>Login</LoginLink>
     </main>
   )
 }
