9501c763523eab8716e3301066904966fc979c27
maritocuate <mario300@gmail.com>
2024-08-08 19:37:03 -0300

    ✨ kinde integration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,18 @@
 'use client'
-import { LoginLink } from '@kinde-oss/kinde-auth-nextjs'
+import {
+  LoginLink,
+  LogoutLink,
+  useKindeBrowserClient,
+} from '@kinde-oss/kinde-auth-nextjs'
 
 export default function Home() {
+  const { isAuthenticated } = useKindeBrowserClient()
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-3xl font-bold">Subscription App</h1>
+      {isAuthenticated ? <p>Authenticated</p> : <p>Not Authenticated</p>}
       <LoginLink>Login</LoginLink>
+      <LogoutLink>Logout</LogoutLink>
     </main>
   )
 }
